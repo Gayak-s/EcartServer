2f1b2546a0b27affbd25e2cb4ccdd0a4b69850d3
Gayak-s <gayaks0.14@gmail.com>
2023-05-04 10:38:38 +0530

    dynamic port setup

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ require('./db/connection')
 //create server application using express
 const server = express()
 
+//create a variable to store port number
+const PORT = 3000 || process.env.PORT
+
 //use in server app
 server.use(cors())
 server.use(express.json())
@@ -26,8 +29,6 @@ server.use(router)
 //     res.status(200).json('E cart Server Started!!!!!')
 // })
 
-//to store port number
-const PORT = 3000
 //run server
 server.listen(PORT,()=>{
     console.log('E cart server started at port',PORT);
